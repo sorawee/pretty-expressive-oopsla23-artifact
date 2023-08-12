f7e47fbe5c488d7fa205a044ad8fc6a8945a58eb
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2023-08-12 15:27:27 -0700

    runnning scripts: refactor

--- scripts/common.rkt
+++ scripts/common.rkt
@@ -1,5 +1,10 @@
 #lang racket
 
-(provide benchmark-result-dir)
+(provide benchmark-result-dir string->nat)
 
 (define benchmark-result-dir (simple-form-path "benchmark-results"))
+
+(define (string->nat #:who who n)
+  (match (string->number n)
+    [(? natural? nn) nn]
+    [_ (raise-user-error (format "~a must be a natural number, got ~a" who n))]))
